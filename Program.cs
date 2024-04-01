82dd32f2cbe96d2bdec1eac4c33e61a68cc82b2b
Maius-Jhenzel-Ponio <165049494+Maius-Jhenzel-Ponio@users.noreply.github.com>
2024-04-01 07:25:15 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.Design;
+
+namespace ACCESS_CONTROL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string RightUsername = "Admin";
+            string RightPassword = "Admin1234!";
+
+            int attempt = 0;
+
+            do
+            {
+                Console.WriteLine("Enter your Username: ");
+                string username = Console.ReadLine();
+                Console.WriteLine("Enter your Password: ");
+                string password = Console.ReadLine();
+
+                if (username == RightUsername && password == RightPassword)
+                {
+                    Console.WriteLine("Login was Succesful!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Login Failed");
+                    attempt++;
+           
+                }
+
+            } while( attempt != 3 );   
+
+            Console.WriteLine("You have used all your attempts!");
+            Console.WriteLine("Access Denied!");
+       
+        }
+    }
+}
